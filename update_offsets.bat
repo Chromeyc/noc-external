ac4acc999a2207de7efd663d1b978b0d27148a2e
Ghost0Dev <119752387+Ghost0Dev@users.noreply.github.com>
2026-01-09 15:17:38 +0100

    Update update_offsets.bat

--- update_offsets.bat
+++ update_offsets.bat
@@ -2,10 +2,8 @@
 echo Updating offsets...
 echo.
 
-REM Get the directory where the batch file is located
 set "SCRIPT_DIR=%~dp0"
 
-REM Use curl to download offsets and save to offsets.h
 curl -o "%SCRIPT_DIR%nocExternal\sdk\offsets.h" "https://imtheo.lol/Offsets/Offsets.hpp"
 
 if %ERRORLEVEL% EQU 0 (
@@ -14,9 +12,10 @@ if %ERRORLEVEL% EQU 0 (
     echo File saved to: %SCRIPT_DIR%nocExternal\sdk\offsets.h
 ) else (
     echo.
-    echo Failed to update offsets. Make sure curl is installed and you have internet connection.
+    echo Failed to update offsets.
 )
 
 echo.
 pause
+
 
